cf5a0b0c38810cc733e45be2faca2a1da2473c2a
Kimame_04 <mendozakyan@gmail.com>
2022-11-28 21:15:25 +0100

    fix: update wtcl engine reference

--- generate_engine_ini.py
+++ generate_engine_ini.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     num_upgrades = int(input("reliability upgrades: "))
     true_reliability = 37000 + 500 * num_upgrades
     with open(f"{series}/{team}.ini", 'w') as f:
-        engine_filenames = {'itcl': 'gt_engine', 'wtcl': 'V8SC_engine'}
+        engine_filenames = {'itcl': 'gt_engine', 'wtcl': 'V8SC_E85_Engine'}
         with open(f'reference_engine_ini/{engine_filenames[series]}.ini') as ref_file:
             for line in ref_file:
                 f.write(line.replace("LifetimeAvg=72000", f"LifetimeAvg={true_reliability}").replace("LifetimeAvg=54000", f"LifetimeAvg={true_reliability}"))
