b4acb609b9cefc6f194fc0b4af931bcbdccdcd7d
Kimame_04 <mendozakyan@gmail.com>
2022-12-06 10:43:45 +0100

    fix: prevent chief engineer effects from increase unreliability

--- generate_engine_ini.py
+++ generate_engine_ini.py
@@ -1,7 +1,10 @@
 
 def generate_engine_ini(team, clasS, ce_multiplier):
     num_upgrades = int(input("reliability upgrades: "))
-    true_reliability = 37000 + 500 * num_upgrades * ce_multiplier
+    if num_upgrades >= 0:
+        true_reliability = 37000 + 500 * num_upgrades * ce_multiplier
+    else: 
+        true_reliability = 37e3 + 500 * num_upgrades
     with open(f"{clasS}/{team}.ini", 'w') as f:
         engine_filenames = {'itcl': 'gt_engine', 'wtcl': 'V8SC_E85_Engine'}
         with open(f'reference_engine_ini/{engine_filenames[clasS]}.ini') as ref_file:
