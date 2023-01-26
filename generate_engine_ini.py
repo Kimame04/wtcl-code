219da447d05efc3307314334dfe8e28ef3e49062
Kimame_04 <mendozakyan@gmail.com>
2023-01-26 16:55:19 +0800

    fix: reliability changes

--- generate_engine_ini.py
+++ generate_engine_ini.py
@@ -1,11 +1,11 @@
 def generate_engine_ini(team, series, ce, num_upgrades):
     ce_multiplier = 1 + ce * 0.01
     if num_upgrades >= 0:
-        true_reliability = 37000 + 500 * num_upgrades * ce_multiplier
+        true_reliability = 18e3 + 500 * num_upgrades * ce_multiplier
     else: 
-        true_reliability = 37000 + 500 * num_upgrades
+        true_reliability = 18e3 + 500 * num_upgrades
     with open(f"{series}/{team}.ini", 'w') as f:
         engine_filenames = {'itcl': 'gt_engine', 'wtcl': 'V8SC_E85_Engine'}
         with open(f'reference_engine_ini/{engine_filenames[series]}.ini') as ref_file:
             for line in ref_file:
-                f.write(line.replace("LifetimeAvg=72000", f"LifetimeAvg={true_reliability}").replace("LifetimeAvg=54000", f"LifetimeAvg={true_reliability}"))
+                f.write(line.replace("LifetimeAvg=72000", f"LifetimeAvg={true_reliability}").replace("LifetimeAvg=54000", f"LifetimeAvg={true_reliability}").replace("LifetimeVar=5000", "LifetimeVar=6000").replace("LifetimeVar=7200", "LifetimeVar=6000"))
